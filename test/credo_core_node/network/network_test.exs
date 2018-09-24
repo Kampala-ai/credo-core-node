3477815f88ae143df85c15436ef17eeb06cf808b
Oleg Kalistratov <oleg.kalistratov@gmail.com>
2018-09-24 17:47:11 +0300

    Tests: connections schema tests

--- test/credo_core_node/network/network_test.exs
+++ test/credo_core_node/network/network_test.exs
@@ -36,4 +36,40 @@ defmodule CredoCoreNode.NetworkTest do
       assert Network.get_known_node(known_node.ip) == nil
     end
   end
+
+  describe "connections" do
+    @describetag table_name: :connections
+    @attrs [ip: "0.0.0.0", is_active: false, failed_attempts_count: 0]
+
+    def connection_fixture(attrs \\ @attrs) do
+      {:ok, connection} =
+        attrs
+        |> Network.write_connection()
+
+      connection
+    end
+
+    test "list_connections/0 returns all connections" do
+      connection = connection_fixture()
+      assert Network.list_connections() == [connection]
+    end
+
+    test "get_connection!/1 returns the connection with given id" do
+      connection = connection_fixture()
+      assert Network.get_connection(connection.ip) == connection
+    end
+
+    test "create_connection/1 with valid data creates a connection" do
+      assert {:ok, connection} = Network.write_connection(@attrs)
+      assert connection.ip == @attrs[:ip]
+      assert connection.is_active == @attrs[:is_active]
+      assert connection.failed_attempts_count == @attrs[:failed_attempts_count]
+    end
+
+    test "delete_connection/1 deletes the connection" do
+      connection = connection_fixture()
+      assert {:ok, connection} = Network.delete_connection(connection)
+      assert Network.get_connection(connection.ip) == nil
+    end
+  end
 end
