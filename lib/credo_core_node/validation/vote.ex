204e9b91d6ffa09c9f55644c6bfb664d12d8ae8e
Stewart Dennis <stewart@bitbounce.com>
2018-10-12 13:00:09 -0700

    Add voting round field.

--- lib/credo_core_node/validation/vote.ex
+++ lib/credo_core_node/validation/vote.ex
@@ -1,3 +1,3 @@
 defmodule CredoCoreNode.Validation.Vote do
-  use Mnesia.Schema, table_name: :votes, fields: [:validator_address, :block_height, :block_hash]
+  use Mnesia.Schema, table_name: :votes, fields: [:validator_address, :block_height, :voting_round, :block_hash]
 end
