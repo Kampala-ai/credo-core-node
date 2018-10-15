55bdc1ad7930ebd36f073647158884e7b5058dd0
Stewart Dennis <stewart@bitbounce.com>
2018-10-15 13:31:41 -0700

    Change produce to propose.

--- lib/credo_core_node/blockchain/block_proposer.ex
+++ lib/credo_core_node/blockchain/block_proposer.ex
@@ -1,30 +1,30 @@
-defmodule CredoCoreNode.Blockchain.BlockProducer do
+defmodule CredoCoreNode.Blockchain.BlockProposer do
   alias CredoCoreNode.Blockchain
   alias CredoCoreNode.Pool
   alias CredoCoreNode.Validation
 
   @doc """
-  Produces the next block if its the node's turn.
+  Proposes the next block if its the node's turn.
 
   To be called after a block is confirmed.
   """
-  def maybe_produce_next_block(confirmed_block) do
+  def maybe_propose_next_block(confirmed_block) do
     if Validation.is_validator?() do
-      if get_next_block_producer(confirmed_block) == Validation.get_own_validator() do
+      if get_next_block_proposer(confirmed_block) == Validation.get_own_validator() do
         Pool.get_batch_of_pending_transactions()
-        |> add_tx_fee_block_producer_reward_transaction()
+        |> add_tx_fee_block_proposer_reward_transaction()
         |> Blockchain.generate_block()
         |> broadcast_block_to_validators()
       else
-        wait_for_block_from_selected_block_producer()
+        wait_for_block_from_selected_block_proposer()
       end
     end
   end
 
   @doc """
-  Adds a transaction to pay transaction fees to the block producer.
+  Adds a transaction to pay transaction fees to the block proposer.
   """
-  def add_tx_fee_block_producer_reward_transaction(transactions) do
+  def add_tx_fee_block_proposer_reward_transaction(transactions) do
     tx_fees_sum =
       Pool.get_pending_transaction_fees_sum(transactions)
 
@@ -41,14 +41,14 @@ defmodule CredoCoreNode.Blockchain.BlockProducer do
   end
 
   @doc """
-  Gets the next block producer.
+  Gets the next block proposer.
 
   #TODO weight by stake size and participation rate.
   """
-  def get_next_block_producer(last_block) do
+  def get_next_block_proposer(last_block) do
     number = last_block.number + 1
 
-    # Seed rand with the current block number to produce a deterministic, pseudorandom result.
+    # Seed rand with the current block number to propose a deterministic, pseudorandom result.
     :rand.seed(:exsplus, {101, 102, number})
 
     index =
@@ -66,11 +66,11 @@ defmodule CredoCoreNode.Blockchain.BlockProducer do
   end
 
   @doc """
-  This will wait, until a specified timeout, for a block to be produced and received by the selected block producer.
-  If a new candidate block is not received by the timeout, a new block producer will be selected.
+  This will wait, until a specified timeout, for a block to be proposed and received by the selected block proposer.
+  If a new candidate block is not received by the timeout, a new block proposer will be selected.
 
-  This is needed in case the selected block producer is offline or otherwise unresponsive.
+  This is needed in case the selected block proposer is offline or otherwise unresponsive.
   """
-  def wait_for_block_from_selected_block_producer do
+  def wait_for_block_from_selected_block_proposer do
   end
 end
