43b5cab5c283a98a1b30da780efeec27d0c2d1de
Stewart Dennis <stewart@bitbounce.com>
2018-10-15 14:28:58 -0700

    Add seeds.

---
+++ priv/repo/seeds.ex
@@ -0,0 +1,2 @@
+CredoCoreNode.Validation.write_validator(%{ip: "1.1.1.1", address: "0x4e0603e2a27a30480e5e3a4fe548e29ef12f64be", stake_amount: 1000, participation_rate: 1, is_self: true})
+CredoCoreNode.Validation.write_validator(%{ip: "1.2.1.2", address: "0x662161927a289eea4dca74392e9c9c4d5755da1a", stake_amount: 2000, participation_rate: 0.8, is_self: false})
