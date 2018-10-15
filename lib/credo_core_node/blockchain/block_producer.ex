eeb8a25354f7292c5d64fc7fc2e07ba8032c6283
Stewart Dennis <stewart@bitbounce.com>
2018-10-15 13:20:25 -0700

    Move block production functionality into a module.

---
+++ lib/credo_core_node/blockchain/block_producer.ex
@@ -0,0 +1,64 @@
+defmodule CredoCoreNode.Blockchain.BlockProducer do
+  alias CredoCoreNode.Blockchain
+  alias CredoCoreNode.Pool
+  alias CredoCoreNode.Validation
+
+  @doc """
+  Broadcasts a block to validators
+  """
+  def broadcast_block_to_validators(block) do
+  end
+
+  @doc """
+  Gets the next block producer.
+
+  #TODO weight by stake size and participation rate.
+  """
+  def get_next_block_producer(last_block) do
+    number = last_block.number + 1
+
+    # Seed rand with the current block number to produce a deterministic, pseudorandom result.
+    :rand.seed(:exsplus, {101, 102, number})
+
+    index =
+      Enum.random(1..Validation.count_validators())
+
+    Validation.list_validators()
+    |> Enum.sort( &(&1.address >= &2.address) )
+    |> Enum.at(index)
+  end
+
+  @doc """
+  Adds a transaction to pay transaction fees to the block producer.
+  """
+  def add_tx_fee_block_producer_reward_transaction(transactions) do
+    transactions
+  end
+
+  @doc """
+  This will wait, until a specified timeout, for a block to be produced and received by the selected block producer.
+  If a new candidate block is not received by the timeout, a new block producer will be selected.
+
+  This is needed in case the selected block producer is offline or otherwise unresponsive.
+  """
+  def wait_for_block_from_selected_block_producer do
+  end
+
+  @doc """
+  Produces the next block if its the node's turn.
+
+  To be called after a block is confirmed.
+  """
+  def maybe_produce_next_block(confirmed_block) do
+    if Validation.is_validator?() do
+      if get_next_block_producer(confirmed_block) == Validation.get_own_validator() do
+        Pool.get_batch_of_pending_transactions()
+        |> add_tx_fee_block_producer_reward_transaction()
+        |> Blockchain.generate_block()
+        |> broadcast_block_to_validators()
+      else
+        wait_for_block_from_selected_block_producer()
+      end
+    end
+  end
+end
--- lib/credo_core_node/blockchain/blockchain.ex
+++ lib/credo_core_node/blockchain/blockchain.ex
@@ -5,8 +5,6 @@ defmodule CredoCoreNode.Blockchain do
 
   alias CredoCoreNode.Blockchain.Block
   alias CredoCoreNode.Blockchain.Transaction
-  alias CredoCoreNode.Pool
-  alias CredoCoreNode.Validation
   alias Mnesia.Repo
 
   @doc """
@@ -93,65 +91,6 @@ defmodule CredoCoreNode.Blockchain do
     }
 
     Map.put(block, :hash, Block.hash(block, encoding: :hex))
-  end
-
-  @doc """
-  Broadcasts a block to validators
-  """
-  def broadcast_block_to_validators(block) do
-  end
-
-  @doc """
-  Gets the next block producer.
-
-  #TODO weight by stake size and participation rate.
-  """
-  def get_next_block_producer(last_block) do
-    number = last_block.number + 1
-
-    # Seed rand with the current block number to produce a deterministic, pseudorandom result.
-    :rand.seed(:exsplus, {101, 102, number})
-
-    index =
-      Enum.random(1..Validation.count_validators())
-
-    Validation.list_validators()
-    |> Enum.sort( &(&1.address >= &2.address) )
-    |> Enum.at(index)
-  end
-
-  @doc """
-  Adds a transaction to pay transaction fees to the block producer.
-  """
-  def add_tx_fee_block_producer_reward_transaction(transactions) do
-    transactions
-  end
-
-  @doc """
-  This will wait, until a specified timeout, for a block to be produced and received by the selected block producer.
-  If a new candidate block is not received by the timeout, a new block producer will be selected.
-
-  This is needed in case the selected block producer is offline or otherwise unresponsive.
-  """
-  def wait_for_block_from_selected_block_producer do
-  end
-
-  @doc """
-  Produces the next block if its the node's turn.
-
-  To be called after a block is confirmed.
-  """
-  def maybe_produce_next_block(confirmed_block) do
-    if Validation.is_validator?() do
-      if get_next_block_producer(confirmed_block) == Validation.get_own_validator() do
-        Pool.get_batch_of_pending_transactions()
-        |> add_tx_fee_block_producer_reward_transaction()
-        |> generate_block()
-        |> broadcast_block_to_validators()
-      else
-        wait_for_block_from_selected_block_producer()
-      end
-    end
   end
 
   @doc """
