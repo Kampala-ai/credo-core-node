d09dac8266e788a46fd901c677ea7028f5cd98a1
Oleg Kalistratov <oleg.kalistratov@gmail.com>
2018-10-17 22:55:46 +0300

    Move initial maybe_update_validator_ip call to Endpoint module

--- lib/credo_core_node/application.ex
+++ lib/credo_core_node/application.ex
@@ -1,8 +1,6 @@
 defmodule CredoCoreNode.Application do
   use Application
 
-  alias CredoCoreNode.Validation.ValidatorIpManager
-
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
@@ -21,8 +19,6 @@ defmodule CredoCoreNode.Application do
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: CredoCoreNode.Supervisor]
-
-    ValidatorIpManager.maybe_update_validator_ip()
 
     Supervisor.start_link(children, opts)
   end
