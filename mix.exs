0712b955026881aebb021617a376cf2343f41cf1
Oleg Kalistratov <oleg.kalistratov@gmail.com>
2018-10-24 22:03:33 +0300

    Update merkle_patricia_tree and remove override for ex_rlp deps

--- mix.exs
+++ mix.exs
@@ -41,9 +41,7 @@ defmodule CredoCoreNode.Mixfile do
       {:poison, "~> 3.1.0"},
       {:hackney, "~> 1.9"},
       {:libsecp256k1, git: "https://github.com/turinginc/libsecp256k1.git", tag: "0.1"},
-      # TODO: remove override once https://github.com/aeternity/elixir-merkle-patricia-tree/pull/18
-      #   is merged
-      {:ex_rlp, "~> 0.3.0", override: true},
+      {:ex_rlp, "~> 0.3.0"},
       {:quantum, "~> 2.3"},
       {:timex, "~> 3.0"},
       {:merkle_patricia_tree, github: "aeternity/elixir-merkle-patricia-tree"},
