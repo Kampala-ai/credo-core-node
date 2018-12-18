a56be91f89c52d295f14177f01f7e9e4b4e84f37
Stewart Dennis <stewart@bitbounce.com>
2018-12-17 16:56:23 -0800

    Block validator tests.

--- test/credo_core_node/blockchain/block_validator_test.exs
+++ test/credo_core_node/blockchain/block_validator_test.exs
@@ -4,6 +4,8 @@ defmodule CredoCoreNode.BlockValidatorTest do
   alias CredoCoreNode.Pool
   alias CredoCoreNode.Blockchain.BlockValidator
 
+  alias Decimal, as: D
+
   describe "validly constructed block" do
     @describetag table_name: :pending_blocks
 
@@ -78,5 +80,145 @@ defmodule CredoCoreNode.BlockValidatorTest do
       refute BlockValidator.validate_coinbase_transaction(invalid_pending_block)
     end
 
+    test "validate_format/1 returns false" do
+      {:ok, invalid_pending_block} =
+        %CredoCoreNode.Pool.PendingBlock{hash: nil}
+        |> Pool.write_pending_block()
+
+      refute BlockValidator.validate_format(invalid_pending_block)
+    end
+
+    test "validate_transaction_amounts/1 returns false" do
+      private_key = :crypto.strong_rand_bytes(32)
+      attrs = [nonce: 0, to: "ABC", value: D.new(1), fee: D.new(1), data: ""]
+
+      {:ok, pending_transaction} =
+        private_key
+        |> Pool.generate_pending_transaction(attrs)
+        |> elem(1)
+        |> Pool.write_pending_transaction()
+
+      {:ok, block} =
+        [pending_transaction]
+        |> Pool.generate_pending_block()
+        |> elem(1)
+        |> Pool.write_pending_block()
+
+      refute BlockValidator.validate_transaction_amounts(block)
+    end
+
+    test "validate_transaction_are_unmined/1 returns false" do
+      pending_block =
+        [
+          struct(
+            CredoCoreNode.Pool.PendingTransaction,
+            data:
+              "{\"tx_type\" : \"security_deposit\", \"node_ip\" : \"10.0.1.9\", \"timelock\": \"\"}",
+            fee: D.new(1.0),
+            hash: "A588D170F64FC3ADAF805670DA67C152FA906B8BB855AAA9B2041ED8E2747FF1",
+            nonce: 0,
+            r: "389576343235F0311A7FA5DD8BCE9C6E529698B66AB146427403C4B6863DC801",
+            s: "46A623CC9B3FAFB41F35A698EE4C7ED73C76FA01D8E12209A76046C0B120D0E9",
+            to: "A9A2B9A1EBDDE9EEB5EF733E47FC137D7EB95340",
+            v: 0,
+            value: D.new(10000.0)
+          )
+        ]
+        |> CredoCoreNode.Pool.generate_pending_block()
+        |> elem(1)
+        |> Pool.write_pending_block()
+        |> elem(1)
+
+      refute BlockValidator.validate_transaction_are_unmined(pending_block)
+    end
+  end
+
+  describe "data length" do
+    @describetag table_name: :pending_blocks
+
+    def block_fixture(data_length) do
+      chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" |> String.split("")
+      long_data =
+        Enum.reduce((0..data_length), [], fn (_i, acc) ->
+          [Enum.random(chars) | acc]
+        end) |> Enum.join("")
+      private_key = :crypto.strong_rand_bytes(32)
+      attrs = [nonce: 0, to: "ABC", value: D.new(1), fee: D.new(1), data: long_data]
+
+      {:ok, pending_transaction} =
+        private_key
+        |> Pool.generate_pending_transaction(attrs)
+        |> elem(1)
+        |> Pool.write_pending_transaction()
+
+      {:ok, block} =
+        [pending_transaction]
+        |> Pool.generate_pending_block()
+        |> elem(1)
+        |> Pool.write_pending_block()
+
+      block
+    end
+
+    test "validate_transaction_data_length/1 returns true" do
+      block = block_fixture(10_000)
+
+      assert BlockValidator.validate_transaction_data_length(block)
+    end
+
+    test "validate_transaction_data_length/1 returns false" do
+      block = block_fixture(60_000)
+
+      refute BlockValidator.validate_transaction_data_length(block)
+    end
+  end
+
+  describe "invalid value transfer limits" do
+    @describetag table_name: :pending_blocks
+    @private_key :crypto.strong_rand_bytes(32)
+    @value D.new(1_000_001)
+    @attrs [nonce: 0, to: "ABC", fee: D.new(1), data: ""]
+
+    def block_fixture(private_key \\ @private_key, attrs \\ @attrs, value \\ @value) do
+      {:ok, pending_transaction} =
+        private_key
+        |> Pool.generate_pending_transaction(attrs ++ [value: value])
+        |> elem(1)
+        |> Pool.write_pending_transaction()
+
+      {:ok, block} =
+        [pending_transaction]
+        |> Pool.generate_pending_block()
+        |> elem(1)
+        |> Pool.write_pending_block()
+
+      block
+    end
+
+    test "validate_value_transfer_limits/1 returns false" do
+      invalid_pending_block = block_fixture()
+
+      refute BlockValidator.validate_value_transfer_limits(invalid_pending_block)
+    end
+
+    test "validate_per_tx_value_transfer_limits/1 returns false" do
+      block = block_fixture()
+      txs = block |> Pool.list_pending_transactions()
+
+      refute BlockValidator.validate_per_tx_value_transfer_limits(txs)
+    end
+
+    test "validate_per_block_value_transfer_limits/1 returns false" do
+      block = block_fixture(@private_key, @attrs, D.new(10_000_001))
+      txs = block |> Pool.list_pending_transactions()
+
+      refute BlockValidator.validate_per_block_value_transfer_limits(txs)
+    end
+
+    test "validate_per_block_chain_segment_value_transfer_limits/1 returns false" do
+      block = block_fixture(@private_key, @attrs, D.new(50_000_001))
+
+      refute BlockValidator.validate_per_block_chain_segment_value_transfer_limits(block)
+    end
   end
 end
