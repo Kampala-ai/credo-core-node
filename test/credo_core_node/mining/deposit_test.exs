522d130732a6b27a2a97d9cae90db764770c0deb
Stewart Dennis <stewart@bitbounce.com>
2018-12-17 22:32:26 -0800

    Tests for processing a miner deposit.

--- test/credo_core_node/mining/deposit_test.exs
+++ test/credo_core_node/mining/deposit_test.exs
@@ -22,8 +22,8 @@ defmodule CredoCoreNode.IpTest do
   describe "validating a miner deposit" do
     @describetag table_name: :miners
 
-    def deposit_fixture(amount, timelock \\ nil) do
-      {:ok, account} = Accounts.generate_address("miner")
+    def deposit_fixture(amount, account \\ nil, timelock \\ nil) do
+      account = account || elem(Accounts.generate_address("miner"), 1)
       tx = Deposit.construct_deposit(amount, account.private_key, account.address, timelock)
     end
 
@@ -37,6 +37,50 @@ defmodule CredoCoreNode.IpTest do
       deposit = deposit_fixture(D.new(5))
 
       assert Deposit.validate_deposits([deposit]) == []
+    end
+  end
+
+  describe "processing a miner deposit" do
+    @describetag table_name: :miners
+
+    def miner_fixture(account) do
+      Mining.write_miner(%{
+        address: account.address,
+        ip: "1.1.1.1",
+        stake_amount: D.new(1_000),
+        participation_rate: 1.0,
+        inserted_at: DateTime.utc_now(),
+        is_self: false
+      })
+      |> elem(1)
+    end
+
+    test "saves the deposit" do
+      deposit = deposit_fixture(D.new(50_000))
+
+      Deposit.recognize_deposits([deposit])
+
+      refute is_nil(Mining.get_deposit(deposit.hash))
+    end
+
+    test "creates a miner if it doesn't exist" do
+      account = elem(Accounts.generate_address("miner"), 1)
+      deposit = deposit_fixture(D.new(50_000), account)
+
+      Deposit.recognize_deposits([deposit])
+
+      refute is_nil(Mining.get_miner(account.address))
+    end
+
+    test "updates a miner if it already exists" do
+      account = elem(Accounts.generate_address("miner"), 1)
+      miner = miner_fixture(account)
+
+      deposit = deposit_fixture(D.new(100_000), account)
+
+      Deposit.recognize_deposits([deposit])
+
+      assert D.cmp(Mining.get_miner(miner.address).stake_amount, D.add(miner.stake_amount, deposit.value)) == :eq
     end
   end
 end
