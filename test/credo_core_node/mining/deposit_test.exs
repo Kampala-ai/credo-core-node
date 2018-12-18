d52b84d47027b15fa80d2c7265d187d137402df4
Stewart Dennis <stewart@bitbounce.com>
2018-12-17 22:24:28 -0800

    Test validating a miner deposit

--- test/credo_core_node/mining/deposit_test.exs
+++ test/credo_core_node/mining/deposit_test.exs
@@ -18,4 +18,25 @@ defmodule CredoCoreNode.IpTest do
       assert Deposit.is_deposit(tx)
     end
   end
+
+  describe "validating a miner deposit" do
+    @describetag table_name: :miners
+
+    def deposit_fixture(amount, timelock \\ nil) do
+      {:ok, account} = Accounts.generate_address("miner")
+      tx = Deposit.construct_deposit(amount, account.private_key, account.address, timelock)
+    end
+
+    test "validates a deposit above the minimum" do
+      deposit = deposit_fixture(D.new(50_000))
+
+      assert Deposit.validate_deposits([deposit]) == [deposit]
+    end
+
+    test "invalidates an overly small deposit" do
+      deposit = deposit_fixture(D.new(5))
+
+      assert Deposit.validate_deposits([deposit]) == []
+    end
+  end
 end
