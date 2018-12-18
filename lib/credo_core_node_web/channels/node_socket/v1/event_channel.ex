4a1cb52f836c1b29a657dfd01f7a24fe07eb51c2
Oleg Kalistratov <oleg.kalistratov@gmail.com>
2018-12-18 23:27:31 +0300

    Block hash/body validations

--- lib/credo_core_node_web/channels/node_socket/v1/event_channel.ex
+++ lib/credo_core_node_web/channels/node_socket/v1/event_channel.ex
@@ -7,9 +7,10 @@ defmodule CredoCoreNodeWeb.NodeSocket.V1.EventChannel do
 
   alias CredoCoreNode.Network
   alias CredoCoreNode.Blockchain
-  alias CredoCoreNode.Blockchain.BlockFragment
+  alias CredoCoreNode.Blockchain.{Block, BlockFragment, Transaction}
   alias CredoCoreNode.Pool
-  alias CredoCoreNode.Pool.PendingBlockFragment
+  alias CredoCoreNode.Pool.{PendingBlock, PendingBlockFragment, PendingTransaction}
+  alias MerklePatriciaTree.Trie
 
   def join("events:all", _message, socket) do
     {:ok, socket}
@@ -29,12 +30,19 @@ defmodule CredoCoreNodeWeb.NodeSocket.V1.EventChannel do
         Logger.warn("Empty body for block #{hash} received, ignoring")
 
       {:ok, body} ->
-        Logger.info("Body for block #{hash} decoded, writing block")
+        Logger.info("Body for block #{hash} decoded")
 
-        hash
-        |> Blockchain.get_block()
-        |> Map.put(:body, body)
-        |> Blockchain.write_block()
+        blk = Blockchain.get_block(hash)
+
+        if valid_block_hash?(blk, hash) && valid_block_body?(blk, body) do
+          Logger.info("Writing block #{hash}")
+
+          blk
+          |> Map.put(:body, body)
+          |> Blockchain.write_block()
+        else
+          Logger.info("Invalid block #{hash}, ignoring")
+        end
 
       _ ->
         Logger.warn("Uprocessable body for block #{hash} received, ignoring")
@@ -72,12 +80,19 @@ defmodule CredoCoreNodeWeb.NodeSocket.V1.EventChannel do
         Logger.warn("Empty body for pending block #{hash} received, ignoring")
 
       {:ok, body} ->
-        Logger.info("Body for pending_block #{hash} decoded, writing pending block")
+        Logger.info("Body for pending_block #{hash} decoded")
 
-        hash
-        |> Pool.get_pending_block()
-        |> Map.put(:body, body)
-        |> Pool.write_pending_block()
+        blk = Pool.get_pending_block(hash)
+
+        if valid_block_hash?(blk, hash) && valid_block_body?(blk, body) do
+          Logger.info("Writing pending_block #{hash}")
+
+          blk
+          |> Map.put(:body, body)
+          |> Pool.write_pending_block()
+        else
+          Logger.info("Invalid pending_block #{hash}, ignoring")
+        end
 
       _ ->
         Logger.warn("Uprocessable body for pending block #{hash} received, ignoring")
@@ -112,4 +127,47 @@ defmodule CredoCoreNodeWeb.NodeSocket.V1.EventChannel do
 
     {:noreply, socket}
   end
+
+  # TODO: refactor block hash/body validation functions for better performance and move to a
+  #   separate module (e.g. BlockValidator)
+  defp valid_block_hash?(nil, _hash), do: false
+  defp valid_block_hash?(%{hash: blk_hash}, hash) when blk_hash != hash, do: false
+  defp valid_block_hash?(blk, hash), do: RLP.Hash.hex(blk) == hash
+
+  defp valid_block_body?(nil, _body), do: false
+
+  defp valid_block_body?(%Block{} = blk, body),
+    do: valid_block_body?(blk, body, Transaction)
+
+  defp valid_block_body?(%PendingBlock{} = blk, body),
+    do: valid_block_body?(blk, body, PendingTransaction)
+
+  defp valid_block_body?(blk, body, tx_module) do
+    try do
+      txs =
+        body
+        |> ExRLP.decode()
+        |> Enum.map(&tx_module.from_list(&1, type: :rlp_default))
+
+      valid_block_transactions?(blk, txs, tx_module)
+    rescue
+      # Body is not properly RLP-encoded
+      ArgumentError -> false
+    end
+  end
+
+  defp valid_block_transactions?(_blk, [], _tx_module), do: false
+
+  defp valid_block_transactions?(blk, txs, tx_module) when is_list(txs) do
+    {:ok, tx_trie, _txs} =
+      MerklePatriciaTree.DB.ETS.random_ets_db()
+      |> Trie.new()
+      |> MPT.Repo.write_list(tx_module, txs)
+
+    tx_root = Base.encode16(tx_trie.root_hash)
+
+    blk.tx_root == tx_root
+  end
+
+  defp valid_block_transactions?(_blk, _invalid_arg, _tx_module), do: false
 end
