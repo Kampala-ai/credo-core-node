398bb1c2836fb08bc95a15d7b130ce3e3a85eec1
Stewart Dennis <stewart@bitbounce.com>
2018-12-20 23:42:37 -0800

    Tests for Mnesia Repo CRUD operations.

--- test/mnesia/repo_test.exs
+++ test/mnesia/repo_test.exs
@@ -5,7 +5,77 @@ defmodule CredoCoreNode.RepoTest do
   alias CredoCoreNode.Accounts
   alias Mnesia.Repo
 
-  describe "limiting list queries" do
+  describe "get record" do
+    @describetag table_name: :accounts
+
+    test "returns a record" do
+      account =
+        Accounts.generate_address()
+        |> elem(1)
+
+      assert Repo.get(Account, account.address) == account
+    end
+  end
+
+  describe "write record" do
+    @describetag table_name: :accounts
+    @private_key :crypto.strong_rand_bytes(32)
+
+    test "persists a record to the database" do
+      address =
+        @private_key
+        |> Accounts.calculate_public_key()
+        |> elem(1)
+        |> Accounts.payment_address()
+
+      {:ok, account} =
+        Accounts.write_account(%{
+          address: address,
+          private_key: @private_key,
+          public_key: nil,
+          label: nil
+        })
+
+      assert Repo.get(Account, account.address) == account
+    end
+  end
+
+  describe "delete record" do
+    @describetag table_name: :accounts
+
+    test "returns nil after the record is deleted" do
+      account =
+        Accounts.generate_address()
+        |> elem(1)
+
+      Repo.delete(account)
+
+      assert Repo.get(Account, account.address) == nil
+    end
+  end
+
+  describe "list records" do
+    @describetag table_name: :accounts
+
+    test "returns a record" do
+      tear_down_accounts()
+
+      account =
+        Accounts.generate_address()
+        |> elem(1)
+
+      account_two =
+        Accounts.generate_address()
+        |> elem(1)
+
+      list = Repo.list(Account)
+
+      assert Enum.member?(list, account)
+      assert Enum.member?(list, account_two)
+    end
+  end
+
+  describe "limit list queries" do
     @describetag table_name: :accounts
 
     def tear_down_accounts do
