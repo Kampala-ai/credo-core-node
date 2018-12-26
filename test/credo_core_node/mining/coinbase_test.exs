3599dbd851fcaaae43b49a1ba15e1eb4b31789ec
Stewart Dennis <stewart@bitbounce.com>
2018-12-25 22:17:45 -0800

    Tests for generating a coinbase transaction.

---
+++ test/credo_core_node/mining/coinbase_test.exs
@@ -0,0 +1,45 @@
+defmodule CredoCoreNode.CoinbaseTest do
+  use CredoCoreNodeWeb.DataCase
+
+  alias CredoCoreNode.Pool
+  alias CredoCoreNode.Mining.Coinbase
+
+  alias Decimal, as: D
+
+  describe "adding a coinbase traansaction" do
+    @describetag table_name: :pending_transactions
+    @private_key :crypto.strong_rand_bytes(32)
+    @attrs [nonce: 0, to: "ABC", value: D.new(1), fee: D.new(1), data: ""]
+
+    def pending_transactions_fixture(private_key \\ @private_key, attrs \\ @attrs) do
+      {:ok, pending_transaction} =
+        private_key
+        |> Pool.generate_pending_transaction(attrs)
+        |> elem(1)
+        |> Pool.write_pending_transaction()
+
+      [pending_transaction]
+    end
+
+    def pending_block_fixture(pending_transactions) do
+      {:ok, pending_block} =
+        pending_transactions
+        |> Pool.generate_pending_block()
+        |> elem(1)
+        |> Pool.write_pending_block()
+
+      pending_block
+    end
+
+    test "adds a valid coinbase transaction" do
+      pending_txs = pending_transactions_fixture()
+      txs = Coinbase.add_coinbase_tx(pending_txs)
+      block = pending_block_fixture(txs)
+
+      [coinbase_tx] = tl(txs)
+
+      assert = Coinbase.is_coinbase_tx(coinbase_tx)
+      assert = Coinbase.tx_fee_sums_match(block, [coinbase_tx])
+    end
+  end
+end
