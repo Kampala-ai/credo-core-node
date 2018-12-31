46e9a88b69e83c3a651089f56b46f06aaddc8cc7
Stewart Dennis <stewart@bitbounce.com>
2018-12-31 00:25:18 -0800

    Always use inclusive offset comparison.

--- lib/credo_core_node_web/controllers/node_api/v1/block_controller.ex
+++ lib/credo_core_node_web/controllers/node_api/v1/block_controller.ex
@@ -16,8 +16,5 @@ defmodule CredoCoreNodeWeb.NodeApi.V1.BlockController do
     render(conn, "index.json", blocks: blocks)
   end
 
-  defp filter_blocks_by_number(blocks, offset) when offset == 0,
-    do: Enum.filter(blocks, &(&1.number >= offset))
-
-  defp filter_blocks_by_number(blocks, offset), do: Enum.filter(blocks, &(&1.number > offset))
+  defp filter_blocks_by_number(blocks, offset), do: Enum.filter(blocks, &(&1.number >= offset))
 end
