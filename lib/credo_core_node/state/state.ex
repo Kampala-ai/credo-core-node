e1c86dec1b9a4ecce3cf7944efc70b5a1491712f
Oleg Kalistratov <oleg.kalistratov@gmail.com>
2019-01-01 20:17:05 +0300

    World state: various fixes/improvements [#11]

--- lib/credo_core_node/state/state.ex
+++ lib/credo_core_node/state/state.ex
@@ -60,13 +60,8 @@ defmodule CredoCoreNode.State do
   Calculates world state for the given block_number, raises errors on fail.
   """
   def calculate_world_state!(end_block_number) when is_integer(end_block_number) do
-    case last_existing_state(end_block_number) do
-      {start_root, ^end_block_number} ->
-        start_root
-
-      {start_root, start_block_number} ->
-        calculate_world_state!(start_root, start_block_number, end_block_number)
-    end
+    {start_root, start_block_number} = last_existing_state(end_block_number)
+    calculate_world_state!(start_root, start_block_number, end_block_number)
   end
 
   @doc """
@@ -76,15 +71,16 @@ defmodule CredoCoreNode.State do
     end_block_number = Blockchain.last_confirmed_block_number()
     {start_root, start_block_number} = last_existing_state(end_block_number)
 
-    start_root =
-      if start_block_number == end_block_number do
-        start_root
-      else
-        calculate_world_state!(start_root, start_block_number, end_block_number)
-      end
-
-    calculate_world_state!(start_root, txs)
+    start_root
+    |> calculate_world_state!(start_block_number, end_block_number)
+    |> calculate_world_state!(txs)
   end
+
+  defp calculate_world_state!(_start_root, _start_block_number, -1), do: nil
+
+  defp calculate_world_state!(start_root, start_block_number, end_block_number)
+       when start_block_number >= end_block_number,
+       do: start_root
 
   defp calculate_world_state!(start_root, start_block_number, end_block_number) do
     blk = Blockchain.get_block_by_number(end_block_number)
@@ -92,7 +88,7 @@ defmodule CredoCoreNode.State do
     blks =
       blk
       |> Blockchain.list_preceding_blocks()
-      |> filter_blocks_by_number(start_block_number)
+      |> Enum.filter(&(&1.number > start_block_number))
       |> List.insert_at(0, blk)
       |> Enum.reverse()
 
@@ -146,6 +142,8 @@ defmodule CredoCoreNode.State do
     Base.encode16(trie.root_hash)
   end
 
+  defp last_existing_state(-1), do: {nil, -1}
+
   defp last_existing_state(block_number) do
     case Blockchain.get_block_by_number(block_number) do
       nil ->
@@ -154,7 +152,7 @@ defmodule CredoCoreNode.State do
       # TODO: backwards-compatibility block, only possible on the current testnet version,
       #   to be removed after moving to mainnet
       %{state_root: ""} ->
-        {nil, 0}
+        {nil, -1}
 
       %{hash: hash, state_root: root} ->
         trie = state_trie(root)
@@ -168,7 +166,7 @@ defmodule CredoCoreNode.State do
 
           Trie.Node.decode_trie(trie) == :empty ->
             Exleveldb.close(elem(trie.db, 1))
-            if block_number > 0, do: last_existing_state(block_number - 1), else: nil
+            if block_number > 0, do: last_existing_state(block_number - 1), else: {nil, -1}
 
           true ->
             Exleveldb.close(elem(trie.db, 1))
@@ -178,10 +176,4 @@ defmodule CredoCoreNode.State do
   end
 
   def state_trie(state_root), do: MPT.RepoManager.trie("state", "state", state_root)
-
-  defp filter_blocks_by_number(blocks, start_block_number) when start_block_number == 0,
-    do: Enum.filter(blocks, &(&1.number >= start_block_number))
-
-  defp filter_blocks_by_number(blocks, start_block_number),
-    do: Enum.filter(blocks, &(&1.number > start_block_number))
 end
